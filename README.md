1d7e54a01358da4030f1b475bd44ac7a8c430168
Pratik Panchal <pratik.panchal@techholding.co>
2023-05-18 16:51:00 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# PKWebservices
+
+A description of this package.
---
+++ Tests/PKWebservicesTests/PKWebservicesTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import PKWebservices
+
+final class PKWebservicesTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(PKWebservices().text, "Hello, World!")
+    }
+}
